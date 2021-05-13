a65baf65376f881028af44d331960807ee3d0370
joao-gino <79086821+joao-gino@users.noreply.github.com>
2021-05-13 20:40:16 -0300

    Update

--- src/DeleteServlet.java
+++ src/DeleteServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//João Vitor Gino CB3005488
+//Douglas William Feu CB3005348
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
